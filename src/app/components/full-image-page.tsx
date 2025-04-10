99de0fcd07f172f990312a4cfd39b06070dc5396
jasper028 <pherdavidpineda@gmail.com>
2025-04-10 10:20:12 +0800

    image showing

--- src/app/components/full-image-page.tsx
+++ src/app/components/full-image-page.tsx
@@ -10,29 +10,34 @@ export default async function FullPageImageView(props: { photoId: string }) {
     const uploaderInfo = await (await clerkClient()).users.getUser(image.userId);
 
     return (
-        <div className="flex h-full w-full min-w-0">
+        <div className="flex h-screen w-full">
             {/* Image Section */}
-            <div className="flex items-center justify-center flex-shrink-0 w-1/2 p-4">
+            <div className="w-1/2 h-full flex-shrink-0">
                 <img
                     src={image.url}
                     alt={image.name}
-                    className="h-auto max-h-[90%] object-contain"
+                    className="w-full h-full object-cover rounded-md"
                 />
             </div>
 
             {/* Content Section */}
-            <div className="flex flex-col w-1/2 border-2 p-4">
-                <div className="border-b p-2 text-center text-lg">{image.name}</div>
-                <div className="flex flex-col p-2">
-                    <span>Uploaded By:</span>
-                    <span>{uploaderInfo.fullName}</span>
-                </div>
-                <div className="flex flex-col p-2">
-                    <span>Created At:</span>
-                    <span>{new Date(image.createdAt).toLocaleDateString()}</span>
+            <div className="flex flex-col w-1/2 border-l p-6 ">
+                <div className="border-b pb-4 text-center text-2xl font-bold text-gray-800">
+                    {image.name}
                 </div>
 
-                <div className="p-2 mt-4">
+                <div className="mt-6 text-lg">
+                    <p className="mb-2">
+                        <span className="font-semibold">Uploaded By:</span>{" "}
+                        {uploaderInfo.fullName}
+                    </p>
+                    <p>
+                        <span className="font-semibold">Created At:</span>{" "}
+                        {new Date(image.createdAt).toLocaleDateString()}
+                    </p>
+                </div>
+
+                <div className="mt-2 pt-4">
                     <form
                         action={async () => {
                             "use server";
